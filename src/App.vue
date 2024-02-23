a54db215269f56f8916545c11c42ee7bcd02e673
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-02-23 15:01:29 -0300

    correção de bug

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,43 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 const contador = ref(0)
-function icrementarContador(){
-  contador.value ++
+function icrementarContador() {
+  contador.value++
 }
-function decrementarContador(){
-  if(contador.value>0){
-  contador.value--
+function decrementarContador() {
+  if (contador.value > 0) {
+    contador.value--
   }
 }
-function resetContador(){
+function resetContador() {
   contador.value = 0
 }
 
 </script>
 
 <template>
-  <button id="icrementar" @click="icrementarContador">+</button>
-  <button id="decrementar" @click="decrementarContador">-</button>
-  <button id="reset" @click="resetContador">Resetar</button>
-  <br>
-  <p>Valor do contador é = {{ contador }}</p>
+<button id="icrementar" @click="icrementarContador">+</button>
+<button id="decrementar" @click="decrementarContador">-</button>
+<button id="reset" @click="resetContador">Resetar</button>
+<br>
+<p>Valor do contador é = {{ contador }}</p>
 </template>
-
 <style scoped>
 button {
   border-radius: 6px
 }
-#icrementar{
-background-color: darkgreen;
-color: white;
+
+#icrementar {
+  background-color: darkgreen;
+  color: white;
 }
-#decrementar{
+
+#decrementar {
   background-color: rgba(255, 45, 45, 0.658);
   color: black;
 }
-#reset{
+
+#reset {
   background-color: rgba(128, 128, 128, 0.637);
   color: rgb(54, 51, 51);
 }
